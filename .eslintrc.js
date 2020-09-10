304d4219002a59be69532a61767060f7275cc5ce
nossbigg <chenggibson@gmail.com>
2020-09-10 13:34:24 +0800

    Add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,18 @@
 const eslintConfig = {
   root: true,
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module",
+    sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
     },
   },
-  extends: ["eslint:recommended", "eslint-config-prettier"],
-};
+  env: {
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'eslint-config-prettier'],
+}
 
 // eslint-disable-next-line
-module.exports = eslintConfig;
+module.exports = eslintConfig
