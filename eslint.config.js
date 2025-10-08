9ff6b567f2340948da32b3e34d423c5d29252f89
nossbigg <chenggibson@gmail.com>
2025-10-08 12:39:40 +1100

    Add eslint-plugin-react-hooks

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,11 @@ import eslint from '@eslint/js';
 import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
 
 export default defineConfig(
   eslint.configs.recommended,
   tseslint.configs.recommended,
   eslintConfigPrettier,
+  eslintPluginReactHooks.configs.recommended,
 );
